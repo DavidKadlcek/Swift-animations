b9cd7ecb0038eb61a8f1540c1b7f1978100db29c
David Kadlček <davidk605@seznam.cz>
2019-01-15 18:22:34 +0100

    Initial Commit

---
+++ AnimateText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimateText
+//
+//  Created by David Kadlček on 15/01/2019.
+//  Copyright © 2019 David Kadlček. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
